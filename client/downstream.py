6a7b46781e69d7a9fa6693e4a47b818953a526a5
X1ang <xl@cumuli-tech.com>
2014-11-03 01:02:22 -0500

    Implemented downstream.py

--- client/downstream.py
+++ client/downstream.py
@@ -1,31 +1,53 @@
 __author__ = 'x1ang.li'
-from urllib2 import urlopen
 
-restreq = urlopen('...')
-response = restreq.read()
+import sys, os
+
+from client.common import *
+
+def updateHostFile():
+    r = reqplain('/silos/'+clicfg['silo_id'])
+    if (r.status_code != 200):
+        raise Exception('HTTP request error. HTTP status code ' + r.status_code)
+    rjson = r.json()
+    if not rjson:
+        raise Exception('Unable to get the response json')
+    if not 'dnsrecords' in rjson:
+        raise Exception('Malformed response. It should contain a "dnsrecord" key')
+    dnsrecordsjson = rjson['dnsrecords']
+
+    if sys.platform == 'win32':
+        hostaddr = '{SystemRoot}\\system32\\drivers\\etc\\hosts' % (os.environ.get('SystemRoot'))
+    elif sys.platform == 'linux':
+        hostaddr = '/etc/hosts'
+    else:
+        raise Exception('The host file\'s address is unknown.')
+
+    #open file and read it, then close.
+    hostfile = open(hostaddr, 'r')
+    curlines = hostfile.readlines() # current lines
+    hostfile.close()
+
+    newlines = list(filter(shouldkeep, curlines))
+    for dnsrecord in dnsrecordsjson:
+        newlines.append('{ip}\t{hostname}'.format(**dnsrecord))
+    newfilestr = os.linesep.join(newlines)
+
+    hostfile = open(hostaddr, 'w')     #open file with argument "w",to empty the file
+    hostfile.write(newfilestr)
+    hostfile.close()
+
+def shouldkeep(line):
+    tokens = line.split()
+    if (len(tokens) < 2):
+        return True
+    hostname = tokens[0]
+    if (hostname in clicfg['hostnames']):
+        return False
+    return True
+
+if __name__ == '__main__':
+    updateHostFile()
+    print('Done updating the host file')
 
 
-if sys.platform == 'win32':
-    hostaddr = 'C:\Windows\System32\drivers\etc\hosts'
-elif sys.platform == 'linux':
-    hostaddr = '/etc/hosts'
-#open file and read it, then close.
-hostfile = open(hostaddr, 'r')
-strlist = hostfile.readlines()
-hostfile.close()
 
-for a_str in strlist[0:]:
-    if a_str.find('gitserver') >= 0 or a_str.find('webserver') >= 0 or a_str.find('dbserver') >= 0 or a_str.find('server') >= 0 or a_str == '\n' or a_str == '\r\n':
-        strlist.remove(a_str)
-strlist.append("\r\n%s gitserver"%myIp)
-strlist.append("\r\n%s webserver"%myIp)
-strlist.append("\r\n%s dbserver"%myIp)
-strlist.append("\r\n%s server"%myIp)
-
-filestr = ''.join(strlist)
-
-#open file with argument "w",to empty the file
-hostfile = open(hostaddr, 'w')
-hostfile.write(filestr)
-hostfile.close()
-print("write < IP to server mapping> to %s !!!"%hostaddr)
